01f5ca8a0cfd0776b532f90bdee51933d44f0ffe
Masaki Kono <masaki19931015@gmail.com>
2021-09-27 22:08:01 +0900

    add:made format like youtube

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.brown,
+        primaryColor: Colors.white,
       ),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'youtube_app'),
@@ -30,15 +30,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final items = List<String>.generate(10, (i) => "Item $i");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Youtube_app'),
-        backgroundColor: Colors.brown,
+        leading: Icon(Icons.videocam),
+        title: const Text(
+          'Flutter大学',
+        ),
+        actions: <Widget>[
+          SizedBox(
+            width: 44,
+            child: ElevatedButton(
+              child: Icon(Icons.search),
+              onPressed: () {
+                //todo
+              },
+            ),
+          ),
+          SizedBox(
+            width: 44,
+            child: ElevatedButton(
+              child: Icon(Icons.more_vert),
+              onPressed: () {
+                //todo
+              },
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Container(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: Image.network(
+                      'https://yt3.ggpht.com/Acyl-4aTTupNoyoxGGRVoC46a-cxqDk1_SC1JiuV-_XeHSeZBZqNJCjLlekP85t7Iw1qqp5aSA=s176-c-k-c0x00ffffff-no-rj'),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Column(
+                  children: <Widget>[
+                    const Text('Flutte大学'),
+                    ElevatedButton(
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.video_call,
+                            color: Colors.red,
+                          ),
+                          Text('登録する'),
+                        ],
+                      ),
+                      onPressed: () {
+                        //
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(items[index]),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
