a7c61b1955f124daca3c8c9740efca38d490a2d9
Masaki Kono <masaki19931015@gmail.com>
2021-10-10 21:50:21 +0900

    done: youtube simulater

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_app/video_detail_page.dart';
 
 void main() {
   runApp(const App());
@@ -69,8 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   width: 60,
                   height: 60,
-                  child: Image.network(
-                      'https://yt3.ggpht.com/Acyl-4aTTupNoyoxGGRVoC46a-cxqDk1_SC1JiuV-_XeHSeZBZqNJCjLlekP85t7Iw1qqp5aSA=s176-c-k-c0x00ffffff-no-rj'),
+                  // child: Image.network(
+                  //     'https://yt3.ggpht.com/Acyl-4aTTupNoyoxGGRVoC46a-cxqDk1_SC1JiuV-_XeHSeZBZqNJCjLlekP85t7Iw1qqp5aSA=s176-c-k-c0x00ffffff-no-rj'),
                 ),
                 const SizedBox(
                   width: 8,
@@ -101,8 +102,43 @@ class _MyHomePageState extends State<MyHomePage> {
                 itemCount: items.length,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text(items[index]),
-                  );
+                      onTap: () async {
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (content) => VideoDetailPage()),
+                        );
+                      },
+                      contentPadding: EdgeInsets.all(8),
+                      leading: Image.network(
+                          'https://yt3.ggpht.com/Acyl-4aTTupNoyoxGGRVoC46a-cxqDk1_SC1JiuV-_XeHSeZBZqNJCjLlekP85t7Iw1qqp5aSA=s176-c-k-c0x00ffffff-no-rj'),
+                      title: Column(
+                        children: [
+                          Text(
+                            '【Flutter超入門】リストを作る方法',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                '267回再生',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Text(
+                                '5日前',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      trailing: Icon(Icons.more_vert));
                 },
               ),
             )
